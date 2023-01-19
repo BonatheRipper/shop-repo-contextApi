98aba17c1dc664d0eeb356411e3d1bba5bb061c8
Izeogu Maduabuchi <53067387+BonatheRipper@users.noreply.github.com>
2023-01-19 14:10:07 +0100

    Update GlobalContext.jsx
    Hi... GreatIgboMan.
     Made some changes in Line 16

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,57 +1,53 @@
-import { useReducer, createContext } from "react"
-import { shopProducts } from "../components/products"
+import { useReducer, createContext } from "react";
+import { shopProducts } from "../components/products";
 
 const initialState = {
-    products: shopProducts,
-    cart: [],
-    amount: 0
-}
+  products: shopProducts,
+  cart: [],
+  amount: 0,
+};
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case "INCREMENT":
-            let eachCart;
-            eachCart = state.products.map((product) => {
-                if (product.id === action.payload) {
-                    return {...product, amount: product.amount + 1}
+  switch (action.type) {
+    case "INCREMENT":
+      let eachCart;
+      eachCart = state.products.map((product) => {
+        if (product.id === action.payload) {
+          return { ...product, amount: (product.amount || 0) + 1 };
+        }
+        return product;
+      });
 
-                }
-                return product
+      return { ...state, products: eachCart, amount: 0 };
 
-                
-            })
+    default:
+      return state;
+  }
+};
 
-            return { ...state, products: eachCart, amount: state.amount + 1}
-
-            
-        default:
-            return state
-    }
-}
-
-const GlobalContext = createContext()
+const GlobalContext = createContext();
 
 export const GlobalProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-    const addOne = (id) => {
-        dispatch({
-            type: "INCREMENT",
-            payload: id
-        })
-    }
+  const addOne = (id) => {
+    dispatch({
+      type: "INCREMENT",
+      payload: id,
+    });
+  };
 
-    return (
-        <GlobalContext.Provider value={{
-            products: state.products,
-            amount: state.amount,
-            addOne
-        }}>
-
-            {children}
-
-        </GlobalContext.Provider>
-    )
-}
+  return (
+    <GlobalContext.Provider
+      value={{
+        products: state.products,
+        amount: state.amount,
+        addOne,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
 
 export default GlobalContext;
